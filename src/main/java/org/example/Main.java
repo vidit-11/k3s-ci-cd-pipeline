cf216bad184910b1f1dba5bb5ecd01f8d681ba0d
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-03-26 11:49:03 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController; // <-- Import this
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController; 
 
 @SpringBootApplication
-@RestController // <-- CRITICAL: You must add this back so Spring registers the @GetMapping methods below
+@RestController 
+@RequestMapping("/api")
 public class Main extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -29,10 +26,8 @@ public class Main extends SpringBootServletInitializer {
         SpringApplication.run(Main.class, args);
     }
 
-    // Since your application.properties has server.servlet.context-path=/api
-    // This will correctly resolve to /api/status
     @GetMapping("/status") 
-    public String getStatus() { // @ResponseBody is implicit with @RestController
+    public String getStatus() { 
         logger.info("Status check requested");
         return "Pipeline working beautifully, backend is running!!!!!!";
     }
